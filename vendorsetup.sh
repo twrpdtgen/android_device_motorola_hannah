a5710a3dc009eb14f9a18532aac7563310a0c83d
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-11-19 07:12:29 +0100

    hannah: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: 1 Manufacturer: motorola Model: Moto E5 Plus Script version: 2.1.0 Signed-off-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo omni_hannah-user
+add_lunch_combo omni_hannah-userdebug
+add_lunch_combo omni_hannah-eng
